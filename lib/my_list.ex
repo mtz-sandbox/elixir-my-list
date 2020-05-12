b3382b9722223c244ed1099e3bc879820a47548e
Santiago Fernandez <sfernandez@matriz.com.ar>
2020-05-12 16:44:04 -0300

    Tests and empty implementation

--- lib/my_list.ex
+++ lib/my_list.ex
@@ -1,18 +1,36 @@
 defmodule MyList do
-  @moduledoc """
-  Documentation for `MyList`.
-  """
+  # Please don't use any external modules (especially List or Enum) in your
+  # implementation. The point of this exercise is to create these basic
+  # functions yourself. You may use basic Kernel functions (like `Kernel.+/2`
+  # for adding numbers), but please do not use Kernel functions for Lists like
+  # `++`, `--`, `hd`, `tl`, `in`, and `length`.
 
-  @doc """
-  Hello world.
+  @spec count(list) :: non_neg_integer
+  def count(l) do
+  end
 
-  ## Examples
+  @spec reverse(list) :: list
+  def reverse(l) do
+  end
 
-      iex> MyList.hello()
-      :world
+  @spec map(list, (any -> any)) :: list
+  def map(l, f) do
+  end
 
-  """
-  def hello do
-    :world
+  @spec filter(list, (any -> as_boolean(term))) :: list
+  def filter(l, f) do
+  end
+
+  @type acc :: any
+  @spec reduce(list, acc, (any, acc -> acc)) :: acc
+  def reduce(l, acc, f) do
+  end
+
+  @spec append(list, list) :: list
+  def append(a, b) do
+  end
+
+  @spec concat([[any]]) :: [any]
+  def concat(ll) do
   end
 end
